33a6a317887b8e8dbe76243cd72a252f10f5c929
Jason Chen <jasonc1@andrew.cmu.edu>
2017-02-17 00:37:52 -0500

    added some scopes to case models and tested a few

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,4 +1,19 @@
 class Case < ActiveRecord::Base
   belongs_to :deacon, class_name: "User", foreign_key: "user_id"
   has_many :votes
+
+  validates_presence_of :client_name, :summary
+  validates_inclusion_of :status, in: %w[submitted review\ in\ progress approved rejected check\ signed check\ processed], message: "is not an option"
+
+  #scopes
+  scope :chronological,    -> { order('date_submitted DESC') }
+  scope :submitted,        -> { where(status: "submitted")}
+  scope :review_in_progress,        -> { where(status: "review in progress")}
+  scope :approved,        -> { where(status: "approved")}
+  scope :rejected,        -> { where(status: "rejected")}
+  scope :check_signed,        -> { where(status: "check signed")}
+  scope :check_processed,        -> { where(status: "check processed")}
+
+  #methods
+
 end
--- test/models/case_test.rb
+++ test/models/case_test.rb
@@ -1,9 +1,24 @@
 require 'test_helper'
 
 class CaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:deacon)
+  should have_many(:votes)
+
+  should validate_presence_of(:client_name)
+  should validate_presence_of(:summary)
+
+  should allow_value("submitted").for(:status)
+  should allow_value("review in progress").for(:status)
+  should allow_value("approved").for(:status)
+  should allow_value("rejected").for(:status)
+  should allow_value("check signed").for(:status)
+  should allow_value("check processed").for(:status)
+  should_not allow_value("bad").for(:status)
+  should_not allow_value("hacker").for(:status)
+  should_not allow_value(10).for(:status)
+  should_not allow_value("vp").for(:status)
+  should_not allow_value(nil).for(:status)
+
   context "Creating a context for users" do
     setup do
       create_users
@@ -22,6 +37,39 @@ class CaseTest < ActiveSupport::TestCase
     end
 
     #running the tests:
+    should "show that the chronological scope works" do
+      assert_equal 7, Case.chronological.size
+      assert_equal ["Gordon Ramsay", "Jonathan Tsao", "Evan Li", "Thomas Lu", "Molly Chou", "Spencer Poon", "Catherine Zeng"], Case.chronological.map{|e| e.client_name}
+    end
 
+    should "show that there is one submitted case " do
+      assert_equal 1, Case.submitted.size
+      assert_equal ["Gordon Ramsay"], Case.submitted.map{|e| e.client_name}
+    end
+
+    should "show that there is one approved case " do
+      assert_equal 1, Case.approved.size
+      assert_equal ["Thomas Lu"], Case.approved.map{|e| e.client_name}
+    end
+
+    should "show that there are 2 review in progress cases " do
+      assert_equal 2, Case.review_in_progress.size
+      assert_equal ["Evan Li","Jonathan Tsao"], Case.review_in_progress.map{|e| e.client_name}.sort
+    end
+
+    should "show that there is one rejected case " do
+      assert_equal 1, Case.rejected.size
+      assert_equal ["Spencer Poon"], Case.rejected.map{|e| e.client_name}
+    end
+
+    should "show that there is one check signed case " do
+      assert_equal 1, Case.check_signed.size
+      assert_equal ["Molly Chou"], Case.check_signed.map{|e| e.client_name}
+    end
+
+    should "show that there is one check processed case " do
+      assert_equal 1, Case.check_processed.size
+      assert_equal ["Catherine Zeng"], Case.check_processed.map{|e| e.client_name}
+    end
   end
 end
