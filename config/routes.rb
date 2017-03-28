f982baf69ee4109d33c3c65331d34661d2f59a87
Jason Chen <jasonc1@andrew.cmu.edu>
2017-03-28 00:01:15 -0400

    formatted add case, worked on some responsive design... some reason

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
 
   get 'home' => 'home#home', as: :home
+  get 'clients' => 'home#clients', as: :clients
+
 
   get 'user/edit' => 'users#edit', :as => :edit_current_user
   get 'signup' => 'users#new', :as => :signup
