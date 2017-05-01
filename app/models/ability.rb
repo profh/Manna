103eb6369214a8d2d299e0b5c3209308165180de
Jason Chen <jasonc1@andrew.cmu.edu>
2017-04-30 20:47:51 -0400

    added some basic viewing/editing/updating/destroying cases

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,11 +11,34 @@ class Ability
 
     elsif user.role? :deacon
       if user.is_care_deacon?
+        #care deacon
+        can :read, Case do |c|
+            c.deacon_id == user.id
+        end
+        can :edit, Case do |c|
+          c.deacon_id == user.id
+        end
+        can :destroy, Case do |c|
+          c.deacon_id == user.id
+        end
+        can :update, Case do |c|
+          c.deacon_id == user.id
+        end
 
+        can :create, Case
       else
-
+        #financial deacon
+        can :create, Vote
+        can :read, Case
+        can :create, Case
+        can :update, Case do |c|
+          c.deacon_id == user.id
+        end
+        can :destroy, Case do |c|
+          c.deacon_id == user.id
+        end
       end
-
+    end
   end
 
 end
